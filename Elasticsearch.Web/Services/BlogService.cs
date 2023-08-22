948edb99a46635ac0c988bfcc8fc361b9df94907
Fevzi Boyukparmaksiz <fevzibparmaksiz@gmail.com>
2023-08-22 16:39:05 +0300

    BlogService sınıfının oluşturulması

---
+++ Elasticsearch.Web/Services/BlogService.cs
@@ -0,0 +1,32 @@
+﻿using Elasticsearch.Web.Models;
+using Elasticsearch.Web.Repositories;
+using Elasticsearch.Web.ViewModels;
+
+namespace Elasticsearch.Web.Services
+{
+    public class BlogService
+    {
+        private readonly BlogRepository _blogRepository;
+
+        public BlogService(BlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public async Task<bool> SaveAsync(BlogCreateViewModel model)
+        {
+            var newBlog = new Blog
+            {
+                Title = model.Title,
+                Content = model.Content,
+                UserId = Guid.NewGuid(),
+                Tags = model.Tags.ToArray()
+            };
+
+            var isCreatedBlog = await _blogRepository.SaveAsync(newBlog);
+
+
+            return isCreatedBlog != null;
+        }
+    }
+}
